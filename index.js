f1b1306f9d6c4e4c548063ebace0e3af638c662e
hovadia <hovadia@outbrain.com>
2016-07-20 19:59:48 +0300

    hello

--- index.js
+++ index.js
@@ -13,10 +13,17 @@ let bot = new Bot({
 
 bot.updateBotConfiguration();
 
+bot.onStartChattingMessage((message) => {
+    bot.getUserProfile(message.from)
+    .then((user) => {
+        message.reply(`Hey ${user.firstName}!`);
+});
+});
+
 bot.onTextMessage((message) => {
     handler.message1();
     message.reply(message.body);
-    message.addResponseKeyboard(["Yes","No"], false, "sexyz22")
+    //message.addResponseKeyboard(["Yes","No"], false, "sexyz22")
 });
 
 // Set up your server and start listening

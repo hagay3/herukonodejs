debbf83fb21ee70d4cd7974ca2d40b2b6a2b5e18
hovadia <hovadia@outbrain.com>
2016-07-20 15:46:49 +0300

    adding bot

--- index.js
+++ index.js
@@ -1,3 +1,31 @@
+
+
+'use strict';
+
+let util = require('util');
+let http = require('http');
+let Bot  = require('@kikinteractive/kik');
+
+// Configure the bot API endpoint, details for your bot
+let bot = new Bot({
+    username: 'echo.bot',
+    apiKey: '8d5fd56b-d95e-476c-ac5b-43d89c4a22ce',
+    baseUrl: 'https://whispering-headland-51239.herokuapp.com/'
+});
+
+bot.updateBotConfiguration();
+
+bot.onTextMessage((message) => {
+    message.reply(message.body);
+});
+
+// Set up your server and start listening
+let server = http
+    .createServer(bot.incoming())
+    .listen(process.env.PORT || 8080);
+
+
+
 var express = require('express');
 var app = express();
 

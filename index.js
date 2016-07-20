82557f5135f98cc6a1bd3f08f514453e8b36ff4d
yatir <yatirb@outbrain.com>
2016-07-20 17:51:28 +0300

    sdfsdf

--- index.js
+++ index.js
@@ -2,18 +2,22 @@
 let util = require('util');
 let http = require('http');
 let Bot = require('@kikinteractive/kik');
+
 // Configure the bot API endpoint, details for your bot
 let bot = new Bot({
-username: 'botmaster36363',
-apiKey: '8d5fd56b-d95e-476c-ac5b-43d89c4a22ce',
-baseUrl: 'https://whispering-headland-51239.herokuapp.com/'
+    username: 'botmaster36363',
+    apiKey: '8d5fd56b-d95e-476c-ac5b-43d89c4a22ce',
+    baseUrl: 'https://whispering-headland-51239.herokuapp.com/'
 });
 bot.updateBotConfiguration();
-bot.onTextMessage((message) => {
-message.reply(message.bosy + message.chatId);
+bot.onTextMessage((message) = > {
+
+
+    message.reply(message.body + " " + message.from +" " + message.id);
 });
 // Set up your server and start listening
 let server = http
-.createServer(bot.incoming())
-.listen(process.env.PORT || 8080);
+    .createServer(bot.incoming())
+    .listen(process.env.PORT || 8080);
+
 

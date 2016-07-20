addfb5eb7ac3c3af741fb19d9c4b516041651a24
Levinsky <imish84@gmail.com>
2016-07-20 20:13:49 +0300

    aads

--- index.js
+++ index.js
@@ -6,27 +6,30 @@ let handler = require('./handler');
 
 // Configure the bot API endpoint, details for your bot
 let bot = new Bot({
-    username: 'botmaster36363',
-    apiKey: '8d5fd56b-d95e-476c-ac5b-43d89c4a22ce',
-    baseUrl: 'https://whispering-headland-51239.herokuapp.com/'
+  username: 'botmaster36363',
+  apiKey: '8d5fd56b-d95e-476c-ac5b-43d89c4a22ce',
+  baseUrl: 'https://whispering-headland-51239.herokuapp.com/'
 });
 
 bot.updateBotConfiguration();
 
 bot.onStartChattingMessage((message) => {
-    bot.getUserProfile(message.from)
+  bot.getUserProfile(message.from)
     .then((user) => {
-        message.reply(`Hey ${user.firstName}!`);
-});
+      message.reply(`Hey ${user.firstName}!`);
+    });
 });
 
 bot.onTextMessage((message) => {
-    handler.message1();
-    message.reply(message.body);
-    //message.addResponseKeyboard(["Yes","No"], false, "sexyz22")
+  message.reply(message.body);
+
+  bot.getUserProfile(message.from)
+    .then((user) => {
+      message.addResponseKeyboard(["Yes", "No"], false, user);
+    });
 });
 
 // Set up your server and start listening
 let server = http
-    .createServer(bot.incoming())
-    .listen(process.env.PORT || 8080);
+  .createServer(bot.incoming())
+  .listen(process.env.PORT || 8080);

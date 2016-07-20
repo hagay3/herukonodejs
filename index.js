8f17544d35d9d97fe68ecffe4fc638ef28fbfec1
hovadia <hovadia@outbrain.com>
2016-07-20 17:11:34 +0300

    sss

--- index.js
+++ index.js
@@ -1,24 +1,19 @@
-
-
-
-
-var express = require('express');
-var app = express();
-
-app.set('port', (process.env.PORT || 8080));
-
-app.use(express.static(__dirname + '/public'));
-
-// views is directory for all template files
-app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
-
-app.get('/', function(request, response) {
-  response.render('pages/index');
+'use strict';
+let util = require('util');
+let http = require('http');
+let Bot = require('@kikinteractive/kik');
+// Configure the bot API endpoint, details for your bot
+let bot = new Bot({
+username: 'botmaster36363',
+apiKey: '8d5fd56b-d95e-476c-ac5b-43d89c4a22ce',
+baseUrl: 'https://whispering-headland-51239.herokuapp.com/'
 });
-
-app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
+bot.updateBotConfiguration();
+bot.onTextMessage((message) => {
+message.reply(message.body);
 });
-
+// Set up your server and start listening
+let server = http
+.createServer(bot.incoming())
+.listen(process.env.PORT || 8080);
 
